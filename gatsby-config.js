d88cd2e26c5ca04a9da5731bfc24530392392f6d
Carys Leung <caryshcleung@gmail.com>
2023-02-27 03:05:07 +0000

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,23 @@ module.exports = {
     title: `Portfolio Gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      icon: `src/images/logo.png`, //Favicon logo
+    },
+},
+]
 };
